1a5e96d6307ebdaf4229f8c44e42f1f7409935fa
Roni <ronisalman@email.arizona.edu>
2020-04-05 21:15:26 -0700

    calculation work

--- calculator.js
+++ calculator.js
@@ -1,9 +1,42 @@
 // Created by Roni Salman
 
+const buttons=document.querySelectorAll(".button");
+const display=document.querySelector("#display");
 
+
+function buttonResponse(){
+
+	for(let i=0;i<buttons.length;i++){
+		const btn=buttons[i];
+		buttons[i].addEventListener("click",function(){
+			if(this.innerHTML==="="){
+				calculate(btn);
+			}
+			else{
+				display.innerHTML+=this.innerHTML;
+			}
+		});
+	}
+
+}
+
+function calculate(btn){
+
+	try{
+		const answer=eval(display.innerHTML);
+		console.log(answer);
+	}
+	catch(error){
+		console.log(error);
+	}
+
+	display.innerHTML="";
+}
 
 
 function init(){
+
+	buttonResponse();
 
 }
 
